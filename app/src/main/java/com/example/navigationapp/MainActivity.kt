a1dbc3a8eb018cdffa7e17b90a0126d50e5d1bd8
AlexSanteramo <santeramoalex01@gmail.com>
2023-01-31 15:10:53 -0500

    map created

--- app/src/main/java/com/example/navigationapp/MainActivity.kt
+++ app/src/main/java/com/example/navigationapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.navigationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mapFragment = supportFragmentManager.findFragmentById((R.id.maps)) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        var mMap = p0
+
+        val mainCampus = LatLng(41.4189,-72.8936)
+        mMap.addMarker(MarkerOptions().position(mainCampus).title("Main Campus"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mainCampus, 16f))
+
     }
 }
