609a1716fcb4b3f9715914f6c0d57bcd216c4320
AlexSanteramo <santeramoalex01@gmail.com>
2023-01-31 16:45:37 -0500

    Added pins, and tested info window

--- app/src/main/java/com/example/navigationapp/MainActivity.kt
+++ app/src/main/java/com/example/navigationapp/MainActivity.kt
@@ -19,9 +19,18 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(p0: GoogleMap) {
         var mMap = p0
-
-        val mainCampus = LatLng(41.4189,-72.8936)
-        mMap.addMarker(MarkerOptions().position(mainCampus).title("Main Campus"))
+        mMap.mapType = GoogleMap.MAP_TYPE_HYBRID
+        val mainCampus = LatLng(41.4189, -72.8936)
+        val CCE = LatLng(41.419641,-72.897404)
+        val tator = LatLng(41.417941,-72.895722)
+        val lender = LatLng(41.4195,-72.8951)
+        val arnold = LatLng(41.418705,-72.894269)
+        val recCenter = LatLng(41.420105,-72.893743)
+        mMap.addMarker(MarkerOptions().position(CCE).title("CCE"))
+        mMap.addMarker(MarkerOptions().position(tator).title("Tator Hall").snippet("Dining Hall"))
+        mMap.addMarker(MarkerOptions().position(lender).title("Lender School of Business"))
+        mMap.addMarker(MarkerOptions().position(arnold).title("Arnold Library"))
+        mMap.addMarker(MarkerOptions().position(recCenter).title("Recreation Center"))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mainCampus, 16f))
 
     }
